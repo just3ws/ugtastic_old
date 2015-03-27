9e33e9f2372919a00df5e9f0eb96ed2da61c7e10
Mike Hall <mike@just3ws.com>
2015-03-27 18:16:38 -0500

    Added tests to YouTube::Playlist

--- app/models/you_tube/playlist.rb
+++ app/models/you_tube/playlist.rb
@@ -2,7 +2,9 @@ module YouTube
   class Playlist < ActiveRecord::Base
     validates :playlist_id, presence: true, uniqueness: true
     validates :etag, presence: true, uniqueness: true
-    validates :title, presence: true, uniqueness: true
+
+    validates :title, presence: true, uniqueness: true, length: { in: 1..100 }, format: { without: /(<|>)/, message: "can't contain anglebrackets" }
+    validates :description, presence: true, length: { in: 1..5000 }, format: { without: /(<|>)/, multiline: true, message: "can't contain anglebrackets" }
 
     has_many :playlist_videos
     has_many :videos, through: :playlist_videos, class_name: 'YouTube::Video'
--- spec/models/you_tube/playlist_spec.rb
+++ spec/models/you_tube/playlist_spec.rb
@@ -1,7 +1,6 @@
-RSpec.describe YouTube::Video, type: :model do
+RSpec.describe YouTube::Playlist, type: :model do
   describe 'validations' do
-    it { should have_many :playlists }
-    it { should have_many :transcripts }
+    it { should have_many :videos }
 
     context 'description' do
       it { should validate_length_of(:description).is_at_least(1) }
@@ -18,25 +17,14 @@ RSpec.describe YouTube::Video, type: :model do
     end
 
     it { should validate_presence_of :etag }
-    it { should validate_presence_of :video_id }
+    it { should validate_presence_of :playlist_id }
 
     context 'uniqueness' do
-      subject { YouTube::Video.new(video_id: 'xxx', etag: 'xxx', title: 'xxx', description: 'xxx') }
+      subject { YouTube::Playlist.new(playlist_id: 'xxx', etag: 'xxx', title: 'xxx', description: 'xxx') }
 
       it { should validate_uniqueness_of :etag }
       it { should validate_uniqueness_of :title }
-      it { should validate_uniqueness_of :video_id }
-    end
-  end
-
-  context '#thumnail' do
-    subject { YouTube::Video.new(cache: { 'snippet' => { 'thumbnails' => { 'default' => {}, 'medium' => {}, 'high' => {}, 'standard' => {} } } })   }
-    it 'returns a default thumbnail' do
-      expect(subject.thumbnail).to be_an OpenStruct
-    end
-
-    it 'fails when given a non-standard thumbnail type' do
-      expect { subject.thumbnail('nonstandard') }.to raise_error 'Unknown thumbnail size "nonstandard".'
+      it { should validate_uniqueness_of :playlist_id }
     end
   end
 end
