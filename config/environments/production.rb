183f556b424ad135b278e79f0fcebba49b73e1a4
Mike Hall <mike@just3ws.com>
2015-03-30 22:45:45 -0500

    Whitespace

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
-
 end
 
 Rails.logger = Le.new(Rails.application.secrets.le_api_token, ssl: true, tag: true)
