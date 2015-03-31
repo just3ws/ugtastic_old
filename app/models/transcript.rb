6d673e9f0002abc95059b3b2a95a2b2441d0125e
Mike Hall <mike@just3ws.com>
2015-03-30 22:17:32 -0500

    Annotated and Rubocop'ed

--- app/models/transcript.rb
+++ app/models/transcript.rb
@@ -3,3 +3,16 @@ class Transcript < ActiveRecord::Base
 
   belongs_to :video, class_name: 'YouTube::Video'
 end
+
+# == Schema Information
+#
+# Table name: transcripts
+#
+#  id         :integer          not null, primary key
+#  locale     :string
+#  video_id   :string
+#  text       :text
+#  format     :integer          default(0)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
