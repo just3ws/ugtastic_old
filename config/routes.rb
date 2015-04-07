c5bf436ee9c9a92b33183bb6cc57348b7c9be27a
Mike Hall <mike@just3ws.com>
2015-04-06 21:46:33 -0500

    Removed destructive actions from the videos controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,35 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   namespace :you_tube do
-    resources :videos
+    resources :videos, only: %i(index show)
   end
   resources :users
   root to: 'you_tube/videos#index'
   get '/auth/:provider/callback' => 'sessions#create'
-  get '/signin' => 'sessions#new', :as => :signin
-  get '/signout' => 'sessions#destroy', :as => :signout
+  get '/signin' => 'sessions#new', as: :signin
+  get '/signout' => 'sessions#destroy', as: :signout
   get '/auth/failure' => 'sessions#failure'
 end
 
 # == Route Map
 #
-#              Prefix Verb   URI Pattern                         Controller#Action
-#         rails_admin        /admin                              RailsAdmin::Engine
-#     you_tube_videos GET    /you_tube/videos(.:format)          you_tube/videos#index
-#                     POST   /you_tube/videos(.:format)          you_tube/videos#create
-#  new_you_tube_video GET    /you_tube/videos/new(.:format)      you_tube/videos#new
-# edit_you_tube_video GET    /you_tube/videos/:id/edit(.:format) you_tube/videos#edit
-#      you_tube_video GET    /you_tube/videos/:id(.:format)      you_tube/videos#show
-#                     PATCH  /you_tube/videos/:id(.:format)      you_tube/videos#update
-#                     PUT    /you_tube/videos/:id(.:format)      you_tube/videos#update
-#                     DELETE /you_tube/videos/:id(.:format)      you_tube/videos#destroy
-#               users GET    /users(.:format)                    users#index
-#                     POST   /users(.:format)                    users#create
-#            new_user GET    /users/new(.:format)                users#new
-#           edit_user GET    /users/:id/edit(.:format)           users#edit
-#                user GET    /users/:id(.:format)                users#show
-#                     PATCH  /users/:id(.:format)                users#update
-#                     PUT    /users/:id(.:format)                users#update
-#                     DELETE /users/:id(.:format)                users#destroy
-#                root GET    /                                   you_tube/videos#index
-#                     GET    /auth/:provider/callback(.:format)  sessions#create
-#              signin GET    /signin(.:format)                   sessions#new
-#             signout GET    /signout(.:format)                  sessions#destroy
-#        auth_failure GET    /auth/failure(.:format)             sessions#failure
+#          Prefix Verb   URI Pattern                        Controller#Action
+#     rails_admin        /admin                             RailsAdmin::Engine
+# you_tube_videos GET    /you_tube/videos(.:format)         you_tube/videos#index
+#  you_tube_video GET    /you_tube/videos/:id(.:format)     you_tube/videos#show
+#           users GET    /users(.:format)                   users#index
+#                 POST   /users(.:format)                   users#create
+#        new_user GET    /users/new(.:format)               users#new
+#       edit_user GET    /users/:id/edit(.:format)          users#edit
+#            user GET    /users/:id(.:format)               users#show
+#                 PATCH  /users/:id(.:format)               users#update
+#                 PUT    /users/:id(.:format)               users#update
+#                 DELETE /users/:id(.:format)               users#destroy
+#            root GET    /                                  you_tube/videos#index
+#                 GET    /auth/:provider/callback(.:format) sessions#create
+#          signin GET    /signin(.:format)                  sessions#new
+#         signout GET    /signout(.:format)                 sessions#destroy
+#    auth_failure GET    /auth/failure(.:format)            sessions#failure
 #
 # Routes for RailsAdmin::Engine:
 #     dashboard GET         /                                      rails_admin/main#dashboard
