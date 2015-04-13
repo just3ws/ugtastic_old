bff9e2939c073ef71821e96314f5c6274dfeed0c
Mike Hall <mike@just3ws.com>
2015-04-12 19:53:33 -0500

    Added search extensions

---
+++ db/migrate/20150413004858_add_searching_helpers.rb
@@ -0,0 +1,6 @@
+class AddSearchingHelpers < ActiveRecord::Migration
+  def change
+    enable_extension 'pg_trgm'
+    enable_extension 'fuzzystrmatch'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413003921) do
+ActiveRecord::Schema.define(version: 20150413004858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
+  enable_extension "fuzzystrmatch"
 
   create_table "friendly_id_slugs", force: :cascade do |t|
     t.string   "slug",                      null: false
