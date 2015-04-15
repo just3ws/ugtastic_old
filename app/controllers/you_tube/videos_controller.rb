daa8f94fe418d02dd8fb11f57c41b734dfa578a6
Mike Hall <mike@just3ws.com>
2015-04-14 21:23:46 -0500

    Tidied a controller

--- app/controllers/you_tube/videos_controller.rb
+++ app/controllers/you_tube/videos_controller.rb
@@ -1,17 +1,10 @@
 module YouTube
   class VideosController < ApplicationController
-    before_action :set_you_tube_video, only: %i(show)
-
     def index
       @you_tube_videos = YouTube::Video.all.page(params[:page])
     end
 
     def show
-    end
-
-    private
-
-    def set_you_tube_video
       @you_tube_video = YouTube::Video.find_by(id: params[:id])
     end
   end
