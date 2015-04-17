84d3556abdde90e7edd0aa169c37a7481171591a
Mike Hall <mike@just3ws.com>
2015-04-17 12:56:24 -0500

    Fixed up Heroku and Oink

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,6 +31,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
+  config.middleware.use( Oink::Middleware, logger: Hodel3000CompliantLogger.new(STDOUT))
 end
 
 Rails.logger = Le.new(Rails.application.secrets.le_api_token, ssl: true, tag: true)
