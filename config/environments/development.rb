2b74384ae993328a6bb32e3edd2315bd6d578791
Mike Hall <mike@just3ws.com>
2015-04-17 12:52:11 -0500

    Added Bullet config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,5 +55,10 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+  end
+
   config.middleware.use Rack::LiveReload
 end
