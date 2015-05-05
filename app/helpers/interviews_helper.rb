0a76a172002814b51b4fda182f32bf2f04831d89
Mike Hall <mike@just3ws.com>
2015-05-05 00:00:33 -0500

    Added metamagic and basic meta tags and twitter card

---
+++ app/helpers/interviews_helper.rb
@@ -0,0 +1,9 @@
+module InterviewsHelper
+  def title_for_interview(you_tube_video)
+    if you_tube_video.at_conference?
+      "Interview with #{you_tube_video.interviewee_names} @ #{you_tube_video.conference_name} #{you_tube_video.conference.city} #{you_tube_video.conference_year}"
+    else
+      "Interview with #{you_tube_video.interviewee_names}"
+    end
+  end
+end
