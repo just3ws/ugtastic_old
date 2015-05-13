56a54064f69d1bcc65d415dcb864d6682dc0c798
Mike Hall <mike@just3ws.com>
2015-05-13 15:33:33 -0500

    Just use the OmniAuth config in Devise

--- config/initializers/omniauth.rb
+++
@@ -1,9 +0,0 @@
-# Rails.application.config.middleware.use OmniAuth::Builder do
-# provider :google_oauth2,
-# Rails.application.secrets.omniauth_provider_key,
-# Rails.application.secrets.omniauth_provider_secret,
-# name: 'google',
-# scope: 'http://gdata.youtube.com',
-# image_aspect_ratio: 'square',
-# image_size: 64
-# end
